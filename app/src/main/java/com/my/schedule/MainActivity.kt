d3c33c8dbf788728f8783b7f0ce2a0044bec56a3
bbayu <bbayu00@gmail.com>
2024-06-25 20:17:46 +0900

    Create Button 1, Button 2
    1. Using Composable class
     1) MyScheduleTheme(MaterialTheme)
     2) Column()
     3) Surface()
     4) Button()
     5) Text()

--- app/src/main/java/com/my/schedule/MainActivity.kt
+++ app/src/main/java/com/my/schedule/MainActivity.kt
@@ -3,13 +3,21 @@ package com.my.schedule
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.my.schedule.ui.theme.MyScheduleTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +26,49 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyScheduleTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Column (modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally){
+                    Surface(modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(), color = MaterialTheme.colorScheme.background) {
+                        Greeting("Android")
+                    }
+
+                    Surface(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                        color = Color.LightGray
+                    ) {
+                        Button(
+                            onClick = { /* 첫 번째 Surface의 버튼 클릭 시 동작 */ },
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Text(text = "Button in Surface 1")
+                        }
+                    }
+
+                    Surface(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                        color = Color.Black
+                    ) {
+                        Button(
+                            onClick = { /* 두 번째 Surface의 버튼 클릭 시 동작 */ },
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Text(text = "Button in Surface 2")
+                        }
+                    }
                 }
+
+
             }
         }
     }
