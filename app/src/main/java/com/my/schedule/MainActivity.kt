53af97ba2597dbfd3470a01e83ef2d16184fa72a
bbayu <bbayu00@gmail.com>
2024-06-27 23:06:19 +0900

    Create HeaderWithButton(), NumberedList()

--- app/src/main/java/com/my/schedule/MainActivity.kt
+++ app/src/main/java/com/my/schedule/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.my.schedule
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,8 +25,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.my.schedule.ui.theme.MyScheduleTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,66 +35,78 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyScheduleTheme {
                 // A surface container using the 'background' color from the theme
-                Column (modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally){
-                    Surface(modifier = Modifier
-                        .padding(8.dp)
-                        .fillMaxWidth(), color = MaterialTheme.colorScheme.background) {
-                        Greeting("Android")
-                    }
-
-                    Surface(
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .fillMaxWidth(),
-                        color = Color.LightGray
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize()
                     ) {
-                        Button(
-                            onClick = { /* 첫 번째 Surface의 버튼 클릭 시 동작 */ },
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .fillMaxWidth()
-                        ) {
-                            Text(text = "Button in Surface 1")
-                        }
-                    }
-
-                    Surface(
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .fillMaxWidth(),
-                        color = Color.Black
-                    ) {
-                        Button(
-                            onClick = { /* 두 번째 Surface의 버튼 클릭 시 동작 */ },
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .fillMaxWidth()
-                        ) {
-                            Text(text = "Button in Surface 2")
-                        }
+                        HeaderWithButton()
+                        NumberedList()
                     }
                 }
+            }
+        }
+    }
 
+    @Composable
+    fun HeaderWithButton() {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "My Schedule",
+                fontSize = 24.sp,
+                modifier = Modifier.weight(1f)
+            )
+            IconButton(
+                onClick = { /* 버튼 클릭 시 동작 */ },
+                modifier = Modifier
+                    .padding(0.dp)
+                    .size(48.dp) // 아이콘 버튼 크기
+            ) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "Favorite", tint = Color.Black)
+            }
+        }
+    }
 
+    @Composable
+    fun NumberedList() {
+        val numbers = (1..100).toList() // 숫자 리스트 생성
+        LazyColumn {
+            items(numbers) { number ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = number.toString(),
+                        fontSize = 24.sp,
+                        modifier = Modifier.weight(1f)
+                    )
+                    Button(
+                        onClick = { /* 버튼 클릭 시 동작 */
+                            Toast.makeText(
+                                baseContext,
+                                "${number.toString()} button Click",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        },
+                        modifier = Modifier.padding(start = 16.dp)
+                    ) {
+                        Text(text = "Button")
+                    }
+                }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyScheduleTheme {
-        Greeting("Android")
-    }
-}
